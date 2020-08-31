a03f126d19133957717a706bc043870059e306b4
John Leintz <johnleintz@scaledflow.com>
2020-08-30 20:33:37 -0500

    added controllers

---
+++ controllers/stocks-controllers.js
@@ -0,0 +1,158 @@
+const HttpError = require('../models/http-error');
+
+const DUMMY_PORTFOLIO_USER = [
+  {
+    user_id: 'jaleintz',
+    first_name: 'John',
+    last_name: 'Leintz',
+    portfolios: [
+  {
+    portfolioId: 'wondertech',
+    stocks: [ 
+      {
+        id: 'tsla',
+        name: 'Tesla'
+      },
+      {
+        id: 'amzn',
+        name: "Amazon"
+      },
+      {
+        id: 'msft',
+        name: 'Microsoft'
+      }
+    ]
+    },
+    {
+      portfolioId: 'airline',
+      stocks: [ 
+        {
+          id: 'dal',
+          name: 'Delta Airlines'
+        },
+        {
+          id: 'aal',
+          name: 'American Airlines'
+        },
+        {
+          id: 'luv',
+          name: 'Southwest Airlines'
+        }
+      ]
+      },
+      {
+        portfolioId: 'legacyauto',
+        stocks: [ 
+          {
+            id: 'gm'
+          },
+          {
+            id: 'f'
+          },
+          {
+            id: 'bmw'
+          }
+        ]
+        }
+      ]
+  },
+
+  {
+    user_id: 'bajohnson',
+    first_name: 'Bob',
+    last_name: 'Johnson',
+    portfolios: [
+  {
+    portfolioId: 'wondertech',
+    stocks: [ 
+      {
+        id: 'tsla',
+        name: 'Tesla'
+      },
+      {
+        id: 'amzn',
+        name: "Amazon"
+      },
+      {
+        id: 'msft',
+        name: 'Microsoft'
+      }
+    ]
+    },
+    {
+      portfolioId: 'airline',
+      stocks: [ 
+        {
+          id: 'dal',
+          name: 'Delta Airlines'
+        },
+        {
+          id: 'aal',
+          name: 'American Airlines'
+        },
+        {
+          id: 'luv',
+          name: 'Southwest Airlines'
+        }
+      ]
+      },
+      {
+        portfolioId: 'legacyauto',
+        stocks: [ 
+          {
+            id: 'gm',
+            name: 'General Motors'
+          },
+          {
+            id: 'f',
+            name: 'Ford Motor'
+          },
+          {
+            id: 'bmw',
+            name: 'Bayerische Motoren Werke'
+          },
+          {
+            id: 'tm',
+            name: 'Toyota Motor'
+          }
+        ]
+        }
+      ]
+  }
+]
+
+// console.log(DUMMY_PORTFOLIO_USER[0]);
+// console.log(DUMMY_PORTFOLIO_USER[0].user_id);
+// console.log(DUMMY_PORTFOLIO_USER[0].portfolios[0]);
+// console.log(DUMMY_PORTFOLIO_USER[0].portfolios[0].stocks[0].id);
+
+
+const getStockByTicker = (req, res, next) => {
+  const tickerId = req.params.tid;
+
+  if (!tickerId) {
+    return res.status('404').json({message: 'Ticker not received'})
+  }
+
+  res.json({stock: tickerId});
+};
+
+const getStockPortfolioByID = (req, res, next) => {
+  const userId = req.params.uid;
+  const user = DUMMY_PORTFOLIO_USER.find(u => {
+    return u.user_id === userId;
+  });
+
+  // Error for middleware
+  if (!user) {
+    return next(
+      new HttpError('Could not find the user_id', 404)
+    );
+  }
+
+  res.json({user: user});
+}
+
+
+exports.getStockByTicker = getStockByTicker;
+exports.getStockPortfolioByID = getStockPortfolioByID;
--- routes/stocks-routes.js
+++ routes/stocks-routes.js
@@ -1,172 +1,17 @@
 const express = require('express');
 
+const stocksControllers = require('../controllers/stocks-controllers');
+
 const router = express.Router();
-
-
-const DUMMY_PORTFOLIO_USER = [
-  {
-    user_id: 'jaleintz',
-    first_name: 'John',
-    last_name: 'Leintz',
-    portfolios: [
-  {
-    portfolioId: 'wondertech',
-    stocks: [ 
-      {
-        id: 'tsla',
-        name: 'Tesla'
-      },
-      {
-        id: 'amzn',
-        name: "Amazon"
-      },
-      {
-        id: 'msft',
-        name: 'Microsoft'
-      }
-    ]
-    },
-    {
-      portfolioId: 'airline',
-      stocks: [ 
-        {
-          id: 'dal',
-          name: 'Delta Airlines'
-        },
-        {
-          id: 'aal',
-          name: 'American Airlines'
-        },
-        {
-          id: 'luv',
-          name: 'Southwest Airlines'
-        }
-      ]
-      },
-      {
-        portfolioId: 'legacyauto',
-        stocks: [ 
-          {
-            id: 'gm'
-          },
-          {
-            id: 'f'
-          },
-          {
-            id: 'bmw'
-          }
-        ]
-        }
-      ]
-  },
-
-  {
-    user_id: 'bajohnson',
-    first_name: 'Bob',
-    last_name: 'Johnson',
-    portfolios: [
-  {
-    portfolioId: 'wondertech',
-    stocks: [ 
-      {
-        id: 'tsla',
-        name: 'Tesla'
-      },
-      {
-        id: 'amzn',
-        name: "Amazon"
-      },
-      {
-        id: 'msft',
-        name: 'Microsoft'
-      }
-    ]
-    },
-    {
-      portfolioId: 'airline',
-      stocks: [ 
-        {
-          id: 'dal',
-          name: 'Delta Airlines'
-        },
-        {
-          id: 'aal',
-          name: 'American Airlines'
-        },
-        {
-          id: 'luv',
-          name: 'Southwest Airlines'
-        }
-      ]
-      },
-      {
-        portfolioId: 'legacyauto',
-        stocks: [ 
-          {
-            id: 'gm',
-            name: 'General Motors'
-          },
-          {
-            id: 'f',
-            name: 'Ford Motor'
-          },
-          {
-            id: 'bmw',
-            name: 'Bayerische Motoren Werke'
-          },
-          {
-            id: 'tm',
-            name: 'Toyota Motor'
-          }
-        ]
-        }
-      ]
-  }
-]
-
-// console.log(DUMMY_PORTFOLIO_USER[0]);
-// console.log(DUMMY_PORTFOLIO_USER[0].user_id);
-// console.log(DUMMY_PORTFOLIO_USER[0].portfolios[0]);
-// console.log(DUMMY_PORTFOLIO_USER[0].portfolios[0].stocks[0].id);
 
 // get stock by ticker
 // http://localhost:5000/api/stocks/appl
-router.get('/:tid', (req, res, next) => {
-  const tickerId = req.params.tid;
-
-  if (!tickerId) {
-    return res.status('404').json({message: 'Ticker not received'})
-  }
-
-  res.json({stock: tickerId});
-});
+router.get('/:tid', stocksControllers.getStockByTicker);
 
 // get portfolio by user id
 // http://localhost:5000/api/stocks/user/jaleintz
 // http://localhost:5000/api/stocks/user/bajohnson
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid;
-  const user = DUMMY_PORTFOLIO_USER.find(u => {
-    return u.user_id === userId;
-  });
-
-  // if (!user) {
-  //   return res.status('404').json({message: 'Could not find user.'})
-  // }
-
-  // Error for middleware
-  if (!user) {
-    const error = new Error('Could not find the provided user_id');
-    error.code = 404;
-    // Asynchronous Error
-     return next(error);
-
-    // Synchronous Error
-    // throw error;
-  }
-
-  res.json({user: user});
-});
+router.get('/user/:uid', stocksControllers.getStockPortfolioByID );
 
 
 module.exports = router;
